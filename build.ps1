9c16c6e28499f318e45670412deaca579a1013ab
Homer Meyer <hmeyer@mersive.com>
2021-01-08 12:33:18 -0700

    Add windows-sdk-10-version-1803-windbg

--- build.ps1
+++ build.ps1
@@ -21,6 +21,7 @@ choco install --no-progress -y git
 choco install --no-progress -y visualstudio2017community
 choco install --no-progress -y visualstudio2017-workload-nativedesktop --package-parameters "--includeOptional"
 choco install --no-progress -y windows-sdk-10.1 --version=10.1.17134.12
+choco install --no-progress -y windows-sdk-10-version-1803-windbg
 $env:PATH="$env:PATH;C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin"
 $env:PATH="$env:PATH;C:\Program Files\Git\bin"
 
