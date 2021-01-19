e7f9c3820d8781d8b944494940927ce158c8e622
Justin McBride <justin@mcbride.io>
2021-01-19 14:38:08 -0700

    try using new image

--- build.ps1
+++ build.ps1
@@ -12,24 +12,8 @@ function ErrorOnExeFailure {
 }
 
 LogBanner "installing dependencies..."
-choco source list
-choco source Add -Name artifactory -Source https://artifactory.mersive.xyz/artifactory/api/nuget/chocolatey
-choco source enable --name artifactory
-choco source disable --name chocolatey
-
-choco install --no-progress -y git
-ErrorOnExeFailure
-choco install --no-progress -y visualstudio2017community
-ErrorOnExeFailure
-choco install --no-progress -y visualstudio2017-workload-nativedesktop --execution-timeout 5400 --package-parameters "--add Microsoft.Component.VC.Runtime.UCRTSDK --add Microsoft.VisualStudio.Component.VC.ATLMFC --add Microsoft.VisualStudio.Component.UWP.Support --add Microsoft.VisualStudio.ComponentGroup.UWP.VC"
-ErrorOnExeFailure
-choco install --no-progress -y windows-sdk-10-version-1803-windbg
-ErrorOnExeFailure
 choco install --no-progress -y conan
 ErrorOnExeFailure
-
-$env:PATH="$env:PATH;C:\Program Files\Git\bin"
-$env:PATH="$env:PATH;C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin"
 $env:PATH="$env:PATH;C:\Program Files\Conan\conan"
 
 git fetch --tags
@@ -52,5 +36,5 @@ ErrorOnExeFailure
 
 LogBanner "Deploying to Artifactory"
 # TODO: Sign into the Artifactory respository correctly
-conan upload "rsusbipclient/$version@ci/stable" --all -c -r mersive
+conan upload "libwebrtc/0.1.0@ci/stable" --all -c -r mersive
 ErrorOnExeFailure
