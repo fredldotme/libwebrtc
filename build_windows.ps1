193f31b6b1faa241e45f9835199202af406026e4
Justin McBride <justin@mcbride.io>
2021-01-21 00:26:42 -0700

    Third Attempt

--- build_windows.ps1
+++ build_windows.ps1
@@ -1,21 +1,23 @@
 function LogBanner($title) {
-    Write-Host "************************************"
-    Write-Host $title
-    Write-Host "************************************"
+  Write-Host "************************************"
+  Write-Host $title
+  Write-Host "************************************"
 }
 
 function ErrorOnExeFailure {
-    if (-not $?)
-    {
-        throw 'Last EXE Call Failed!'
-    }
+  if (-not $?)
+  {
+    throw 'Last EXE Call Failed!'
+  }
 }
 
 LogBanner "installing dependencies..."
-choco source list
 choco source Add -Name artifactory -Source https://artifactory.mersive.xyz/artifactory/api/nuget/chocolatey
+ErrorOnExeFailure
 choco source enable --name artifactory
+ErrorOnExeFailure
 choco source disable --name chocolatey
+ErrorOnExeFailure
 
 choco install --no-progress -y git
 ErrorOnExeFailure
@@ -35,23 +37,31 @@ $env:PATH="$env:PATH;C:\Program Files\Conan\conan"
 git fetch --tags
 ErrorOnExeFailure
 
-LogBanner "running cmake..."
+LogBanner "Configuring with CMake"
 mkdir out
 cd out
 cmake .. -G "Visual Studio 15 2017" -DTARGET_CPU=x86
 ErrorOnExeFailure
 
-LogBanner "running msbuild..."
+LogBanner "Performing Build"
 msbuild libwebrtc.sln /p:Configuration=Release /p:Platform=Win32 /target:ALL_BUILD
 ErrorOnExeFailure
 
 LogBanner "Creating conan package from recipe..."
 cd ..
-conan export-pkg . "libwebrtc/0.1.0" -s os="Windows" -s arch="x86"
+conan export-pkg . "libwebrtc/0.1.0@" -s arch=x86
 ErrorOnExeFailure
 
 LogBanner "Deploying to Artifactory"
+Write-Host "Configuring Conan: Adding Mersive Artifactory"
 conan remote add mersive https://artifactory.mersive.xyz/artifactory/api/conan/conan-mersive
-conan user "ci-libwebrtc" -r mersive -p "$env:ARTIFACTORY_PASSWORD"
+ErrorOnExeFailure
+Write-Host "Configuring Conan: Removing conan-center"
+conan remote remove conan-center
+ErrorOnExeFailure
+Write-Host "Configuring Conan: Logging into Mersive Artifactory"
+conan user -p
+ErrorOnExeFailure
+Write-Host "Performing conan upload"
 conan upload "libwebrtc/0.1.0@" --all -c -r mersive
 ErrorOnExeFailure
--- conanfile.py
+++ conanfile.py
@@ -1,15 +1,15 @@
 from conans import ConanFile, tools
 
 class LibwebrtcConan(ConanFile):
-    name = "libwebrtc"
-    version = "0.1.0"
-    settings = "os", "arch"
-    description = "Libwebrtc"
-    url = "https://github.com/Mersive-Technologies/libwebrtc"
-    license = "Apache License 2.0" # TODO: Perhaps work out the legal ramifications later.
-    author = "Mersive Technologies"
-    topics = None
+  name = "libwebrtc"
+  version = "0.1.0"
+  settings = "os", "arch"
+  description = "Libwebrtc"
+  url = "https://github.com/Mersive-Technologies/libwebrtc"
+  license = "Apache License 2.0" # TODO: Perhaps work out the legal ramifications later.
+  author = "Mersive Technologies"
+  topics = None
 
-    def package(self):
-        self.copy("*.h", src="out/webrtc", dst="webrtc")
-        self.copy("*.lib", src="out/lib", dst="lib")
+  def package(self):
+    self.copy("*.h", src="out/webrtc", dst="webrtc")
+    self.copy("*.lib", src="out/lib", dst="lib")
