5fb23b3707b0c69fb4e5874c370c1f120a26a9d9
Justin McBride <justin@mcbride.io>
2021-01-25 21:29:51 -0700

    break out more steps

--- build_windows.ps1
+++ build_windows.ps1
@@ -1,9 +1,3 @@
-function LogBanner($title) {
-  Write-Host "************************************"
-  Write-Host $title
-  Write-Host "************************************"
-}
-
 function ErrorOnExeFailure {
   if (-not $?)
   {
@@ -11,34 +5,12 @@ function ErrorOnExeFailure {
   }
 }
 
-git fetch --tags
-ErrorOnExeFailure
-
-$commitHash = git rev-parse HEAD
-ErrorOnExeFailure
-
-$libwebrtcVersion = (Get-Content -Path version.txt).TrimEnd()
-
-LogBanner "Configuring with CMake"
+Write-Host "Configuring with CMake"
 mkdir out
 cd out
 cmake .. -G "Visual Studio 15 2017" -DTARGET_CPU=x86
 ErrorOnExeFailure
 
-LogBanner "Performing Build"
+Write-Host "Performing Build"
 msbuild libwebrtc.sln /p:Configuration=Release /p:Platform=Win32 /target:ALL_BUILD
-ErrorOnExeFailure
-
-LogBanner "Creating conan package from recipe..."
-cd ..
-((Get-Content -Path conandata.yml) -Replace 'replace_with_commit_hash', $commitHash) | Set-Content -Path conandata.yml
-conan export-pkg . "libwebrtc/${libwebrtcVersion}@" -s arch=x86
-ErrorOnExeFailure
-
-LogBanner "Deploying to Artifactory"
-Write-Host "Configuring Conan: Logging into Mersive Artifactory"
-conan user -p
-ErrorOnExeFailure
-Write-Host "Performing conan upload"
-conan upload "libwebrtc/${libwebrtcVersion}@" --all -c -r mersive
 ErrorOnExeFailure
